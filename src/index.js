d4141642df73a00c2163d025a1ca4a2bf8f065c8
GabrielFelix23 <srgfelix123@gmail.com>
2021-01-15 12:37:02 -0300

    Trocando rota da api para port 3333

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ server.use(express.json())
 const Router = require('./router/taskRouter')
 server.use('/task', Router)
 
-server.listen(3000, () => {
+server.listen(3333, () => {
     console.log("Server Online!")
 })
